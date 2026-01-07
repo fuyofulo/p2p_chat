b74c6660533b659e31933f2a238a5906eb918e99
Zaid Khan <fuyofulo@Zaids-MacBook-Pro.local>
2026-01-08 03:43:53 +0530

    iroh

---
+++ src/main.rs
@@ -0,0 +1,158 @@
+use std::collections::HashMap;
+use std::{fmt, vec};
+use std::str::FromStr;
+use anyhow::Result;
+use futures_lite::StreamExt;
+use iroh::{Endpoint, SecretKey};
+use iroh::protocol::Router;
+use iroh_gossip::api::{GossipReceiver, Event};
+use iroh_gossip::proto::TopicId;
+use iroh_gossip::net::Gossip;
+use iroh::EndpointId;
+use iroh::EndpointAddr;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Message {
+    body: MessageBody,
+    nonce: [u8; 16]
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum MessageBody {
+    AboutMe { from: EndpointId, name: String},
+    Message { from: EndpointId, text: String }
+}
+
+impl Message {
+    fn from_bytes(bytes: &[u8]) -> Result<Self> {
+        serde_json::from_slice(bytes).map_err(Into::into)
+    }
+    
+    pub fn new(body: MessageBody) -> Self {
+        Self {
+            body,
+            nonce: rand::random()
+        }
+    }
+    pub fn to_vec(&self) -> Vec<u8> {
+        serde_json::to_vec(self).expect("serde_json::to_vec is infallible")
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Ticket {
+    topic: TopicId,
+    endpoints: Vec<EndpointAddr>
+}
+
+impl Ticket {
+    fn from_bytes(bytes: &[u8]) -> Result<Self> {
+        serde_json::from_slice(bytes).map_err(Into::into)
+    }
+    pub fn to_bytes(&self) -> Vec<u8> {
+        serde_json::to_vec(self).expect("serde_json::to_vec is infallible")
+    }
+}
+
+impl fmt::Display for Ticket {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut text = data_encoding::BASE32_NOPAD.encode(&self.to_bytes()[..]);
+        text.make_ascii_lowercase();
+        write!(f, "{}", text)
+    }
+}
+
+// The `FromStr` trait allows us to turn a `str` into
+// a `Ticket`
+impl FromStr for Ticket {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let bytes = data_encoding::BASE32_NOPAD.decode(s.to_ascii_uppercase().as_bytes())?;
+        Self::from_bytes(&bytes)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    
+    let secret_key = SecretKey::generate(&mut rand::rng());
+    
+    let endpoint = Endpoint::builder()
+        .secret_key(secret_key)
+        .bind()
+        .await?;
+    
+    println!("> our endpoint id: {}", endpoint.id());
+    
+    let gossip = Gossip::builder().spawn(endpoint.clone());
+    
+    let router = Router::builder(endpoint.clone())
+        .accept(iroh_gossip::ALPN, gossip.clone())
+        .spawn();
+    
+    let id = TopicId::from_bytes(rand::random());
+    let ticket = {
+        let me = endpoint.addr();
+        let endpoints = vec![me];
+        Ticket { topic: id, endpoints }
+    };
+    println!("> ticket to join us: {ticket}");
+    
+    let endpoint_ids = vec![];
+    
+    let topic = gossip.subscribe(id, endpoint_ids).await?;
+    
+    let (sender, receiver) = topic.split();
+    
+    let message = Message::new(MessageBody::AboutMe { from: endpoint.id(), name: String::from("fuyo") });
+    
+    sender.broadcast(message.to_vec().into()).await?;
+    
+    tokio::spawn(subscribe_loop(receiver));
+    
+    let (line_tx, mut line_rx) = tokio::sync::mpsc::channel(1);
+    std::thread::spawn(move || input_loop(line_tx));
+    
+    println!("> type a message and hit enter to broadcast: ");
+    while let Some(text) = line_rx.recv().await {
+        let message = Message::new(MessageBody::Message { from: endpoint.id(), text: text.clone() });
+        sender.broadcast(message.to_vec().into()).await?;
+        println!("> sent: {text}");
+    }
+    
+    router.shutdown().await?;
+    
+    Ok(())
+}
+
+async fn subscribe_loop(mut receiver: GossipReceiver) -> Result<()> {
+    let mut names = HashMap::new();
+    while let Some(event) = receiver.try_next().await? {
+        if let Event::Received(msg) = event {
+            match Message::from_bytes(&msg.content)?.body {
+                MessageBody::AboutMe { from, name } => {
+                    names.insert(from, name.clone());
+                    println!("> {} is now known as {}", from.fmt_short(), name);
+                }
+                MessageBody::Message { from, text } => {
+                    let name = names
+                        .get(&from)
+                        .map_or_else(|| from.fmt_short().to_string(), String::to_string);
+                    println!("{}: {}", name, text);
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+fn input_loop(line_tx: tokio::sync::mpsc::Sender<String>) -> Result<()> {
+    let mut buffer = String::new();
+    let stdin = std::io::stdin();
+    loop {
+        stdin.read_line(&mut buffer)?;
+        line_tx.blocking_send(buffer.clone())?;
+        buffer.clear();
+    }
+}
